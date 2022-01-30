6eefedac3d8c5cf41914ed90067c6a0e70fdc61a
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-01-31 06:35:07 +0900

    Study : RPS, Finonacci

--- Array/src/RPS.java
+++ Array/src/RPS.java
@@ -28,6 +28,14 @@ import java.util.Scanner;
 public class RPS {
     public String solution(int n, int[] a, int[] b) {
         String answer = "";
+        for (int i = 0; i < n; i++) {
+            if (a[i] == b[i]) {
+                answer += "D";
+            }else if (a[i] == 1 && b[i] ==3 ) answer += "A";
+            else if (a[i] == 2 && b[i] ==1 ) answer += "A";
+            else if (a[i] == 3 && b[i] ==2 ) answer += "A";
+            else answer += "B";
+        }
 
         return answer;
     }
@@ -44,6 +52,7 @@ public class RPS {
         for (int i = 0; i < n; i++) {
             b[i] = kb.nextInt();
         }
-        System.out.print(rps.solution(n, a, b));
+        for (char x : rps.solution(n,a,b).toCharArray()) System.out.println(x);
+
     }
 }
