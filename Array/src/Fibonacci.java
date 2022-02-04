98e5c7648011b629045fad269c026b69d9d4f2d3
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-02-04 19:54:32 +0900

    Study: Sieve Of Eratosthenes

--- Array/src/Fibonacci.java
+++ Array/src/Fibonacci.java
@@ -15,19 +15,32 @@ import java.util.Scanner;
  */
 
 public class Fibonacci {
-    public int[] solution(int n){
-        int[] answer=new int[n];
-        answer[0]=1;
-        answer[1]=1;
-        for(int i=2; i<n; i++){
-            answer[i]=answer[i-2]+answer[i-1];
+    public void solution(int n){
+        /**
+         * 배열 활용
+         * int[] answer=new int[n];
+         * answer[0]=1;
+         * answer[1]=1;
+         * for(int i=2; i<n; i++){
+         *     answer[i]=answer[i-2]+answer[i-1];}
+         * return answer;
+         */
+
+        //손 코딩
+        int a = 1, b = 1, c;
+        System.out.print(a + " " + b + " ");
+        for (int i = 2; i < n; i++) {
+            c = a + b;
+            System.out.print(c + " ");
+            a = b;
+            c = b;
         }
-        return answer;
     }
     public static void main(String[] args){
-        Fibonacci T = new Fibonacci();
+        Fibonacci fibonacci = new Fibonacci();
         Scanner kb = new Scanner(System.in);
         int n=kb.nextInt();
-        for(int x :T.solution(n)) System.out.print(x+" ");
+        //for(int x :T.solution(n)) System.out.print(x+" ");
+        fibonacci.solution(n);
     }
 }
---
+++ Array/src/ScoreAC.java
@@ -0,0 +1,2 @@
+public class ScoreAC {
+}
