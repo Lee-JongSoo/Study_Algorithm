4b20be94a25a29b15e2d6ebbc62201119ae19ff3
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-02-14 12:15:51 +0900

    Study: Binary Search Algorithm

--- Sorting/src/BinarySearchA.java
+++ Sorting/src/BinarySearchA.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * 이분검색
  * 임의의 N개의 숫자가 입력으로 주어집니다. N개의 수를 오름차순으로 정렬한 다음 N개의 수 중 한 개의 수인 M이 주어지면
@@ -15,5 +18,34 @@
  * 예시 출력 3
  */
 
-public class BinarySearch {
+public class BinarySearchA {
+
+    public int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Arrays.sort(arr);
+        int lt = 0, rt = n - 1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            }
+            if (arr[mid] > m) rt = mid - 1;
+            else lt = mid + 1;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        BinarySearchA binarySearchA = new BinarySearchA();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        System.out.println(binarySearchA.solution(n, m, arr));
+    }
 }
