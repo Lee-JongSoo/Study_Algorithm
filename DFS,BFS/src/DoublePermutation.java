020e25be22c1a76212f1665abaea0401ce964026
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-02-23 14:53:16 +0900

    Study: DFS

--- DFS,BFS/src/DoublePermutation.java
+++ DFS,BFS/src/DoublePermutation.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  1부터 N까지 번호가 적힌 구슬이 있습니다. 이 중 중복을 허락하여 M번을 뽑아 일렬로 나열 하는 방법을 모두 출력합니다.
 
@@ -22,6 +24,32 @@
  */
 
 public class DoublePermutation {
+
+    static int[] pm;
+    static int n, m;
+
+    public void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        } else {
+            for (int i = 1; i <= n; i++) {
+                pm[L] = i;
+                DFS(L+1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublePermutation doublePermutation = new DoublePermutation();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        pm = new int[m];
+        doublePermutation.DFS(0);
+    }
 
 
 }
---
+++ DFS,BFS/src/Permutation.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+
+/**
+ 10이하의 N개의 자연수가 주어지면 이 중 M개를 뽑아 일렬로 나열하는 방법을 모두 출력합니다.
+
+ ▣ 입력설명
+ 첫 번째 줄에 자연수 N(3<=N<=10)과 M(2<=M<=N) 이 주어집니다. 두 번째 줄에 N개의 자연수가 오름차순으로 주어집니다.
+
+ ▣ 출력설명
+ 첫 번째 줄에 결과를 출력합니다.
+ 출력순서는 사전순으로 오름차순으로 출력합니다.
+
+ ▣ 입력예제 3 2
+          3 6 9
+ ▣ 출력예제
+ 3 6
+ 3 9
+ 6 3
+ 6 9
+ 9 3
+ 9 6
+ */
+
+public class Permutation {
+
+    static int[] pm, ch, arr;
+    static int n, m;
+
+    public void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+            }
+            System.out.println();
+        }
+        else {
+            for (int i = 0; i < n; i++) {
+                if (ch[i] == 0) {
+                    ch[i] = 1;
+                    pm[L] = arr[i];
+                    DFS(L + 1);
+                    ch[i] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Permutation permutation = new Permutation();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        ch = new int[n];
+        pm = new int[m];
+        permutation.DFS(0);
+    }
+}
