ba5f37e06939004c575ba0d0f7c75dea3669a09f
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-02-28 13:52:04 +0900

    Study: DFS & BFS

--- DFS,BFS/src/Maze.java
+++ DFS,BFS/src/Maze.java
@@ -2,12 +2,25 @@ import java.util.Scanner;
 
 public class Maze {
 
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, 1, 0, -1};
     static int[][] board;
     static int answer = 0;
 
     public void DFS(int x, int y) {
+        if (x == 7 && y == 7) {
+            answer++;
+        } else {
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 1 && nx <= 7 && ny >= 1 && ny <= 7 && board[nx][ny] == 0) {
+                    board[nx][ny] = 1;
+                    DFS(nx, ny);
+                    board[nx][ny] = 0;
+                }
+            }
+        }
 
     }
 
