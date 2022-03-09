482635ed070af92ddc9ee6ab26145e8e9b2b200e
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-09 13:03:19 +0900

    Study: PriotiyQueue & Dijkstra Algorithm

---
+++ Greedy/src/PriorityQueueA.java
@@ -0,0 +1,85 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+/**
+ * 최대 수입 스케쥴(PriorityQueue 응용문제)
+ * 현수는 유명한 강연자이다. N개이 기업에서 강연 요청을 해왔다. 각 기업은 D일 안에 와서 강연을 해 주면 M만큼의 강연료를 주기로 했다.
+ * 각 기업이 요청한 D와 M를 바탕으로 가장 많을 돈을 벌 수 있도록 강연 스케쥴을 짜야 한다.
+ * 단 강연의 특성상 현수는 하루에 하나의 기업에서만 강연을 할 수 있다.
+ * <p>
+ * 입력
+ * 첫 번째 줄에 자연수 N(1<=N<=10,000)이 주어지고, 다음 N개의 줄에 M(1<=M<=10,000)과 D(1<=D<=10,000)가 차례로 주어진다.
+ * <p>
+ * 출력
+ * 첫 번째 줄에 최대로 벌 수 있는 수입을 출력한다.
+ * <p>
+ * <p>
+ * 예시 입력 1
+ * 6
+ * 50 2
+ * 20 1
+ * 40 2
+ * 60 3
+ * 30 3
+ * 30 1
+ * 예시 출력 1
+ *
+ * 150
+ */
+
+class Lecture implements Comparable<Lecture>{
+
+    public int money;
+    public int date;
+
+    Lecture(int money, int date) {
+        this.money = money;
+        this.date = date;
+    }
+    @Override
+    public int compareTo(Lecture ob){
+        return ob.date-this.date;
+    }
+}
+
+
+public class PriorityQueueA {
+
+    static int n, max = Integer.MIN_VALUE;
+
+    public int solution(ArrayList<Lecture> arr){
+        int answer=0;
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+        Collections.sort(arr);
+        int j = 0;
+        for (int i = max; i >= 1; i--) {
+            for (; j < n; j++) {
+                if (arr.get(j).date < i) break;
+                pQ.offer(arr.get(j).money);
+            }
+            if (!pQ.isEmpty()) answer += pQ.poll();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        PriorityQueueA pqa = new PriorityQueueA();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        ArrayList<Lecture> arr = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int m = sc.nextInt();
+            int d = sc.nextInt();
+            arr.add(new Lecture(m, d));
+            if (d > max) {
+                max = d;
+            }
+        }
+        System.out.println(pqa.solution(arr));
+
+    }
+
+}
