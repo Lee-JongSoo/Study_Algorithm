681cacfaaef4935c91e8a0dd61b9ba27f1ad218a
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-10 18:42:16 +0900

    Study: Union Find & Kruskal & Prim Algorithm

--- Greedy/src/UnionFindA.java
+++ Greedy/src/UnionFindA.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  친구인가? (Disjoint-Set : Union&Find)
  설명
@@ -40,5 +42,42 @@
  NO
  */
 
-public class DisjointSetUnionFind {
+public class UnionFindA {
+
+    static int[] unf;
+
+    public static int Find(int v) {
+        if (v == unf[v]) return v;   //학생의 집합 번호를 return
+        else return unf[v] = Find(unf[v]);
+    }
+
+    public static void Union(int a, int b) {
+        int fa = Find(a);
+        int fb = Find(b);
+        if (fa != fb) unf[fa] = fb;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        unf = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            unf[i] = i;
+        }
+        for (int i = 1; i <= m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            Union(a, b);
+        }
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int fa = Find(a);
+        int fb = Find(b);
+
+        if (fa == fb) System.out.println("YES");
+        else System.out.println("NO");
+    }
 }
