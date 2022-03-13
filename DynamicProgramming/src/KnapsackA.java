1077bf063d16757a0e78cf6fb36b02693b00525f
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-13 19:22:16 +0900

    Study: Knapsack Algorithm

--- DynamicProgramming/src/KnapsackA.java
+++ DynamicProgramming/src/KnapsackA.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  동전교환(냅색 알고리즘)
  다음과 같이 여러 단위의 동전들이 주어져 있을때 거스름돈을 가장 적은 수의 동전으로 교환해주려면 어떻게 주면 되는가?
@@ -19,4 +22,34 @@
  */
 
 public class KnapsackA {
+
+    static int n, m;
+    static int[] dy;
+
+    public int solution(int[] coin) {
+        Arrays.fill(dy, Integer.MAX_VALUE);
+        dy[0] = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = coin[i]; j <= m; j++) {
+                dy[j] = Math.min(dy[j], dy[j - coin[i]] + 1);
+            }
+        }
+
+        return dy[m];
+    }
+
+    public static void main(String[] args) {
+        KnapsackA knapsackA = new KnapsackA();
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        m = sc.nextInt();
+        dy = new int[m + 1];
+        System.out.println(knapsackA.solution(arr));
+    }
 }
--- DynamicProgramming/src/KnapsackA2.java
+++ DynamicProgramming/src/KnapsackA2.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  최대점수 구하기(냅색 알고리즘)
  이번 정보올림피아드대회에서 좋은 성적을 내기 위하여 현수는 선생님이 주신 N개의 문제를 풀려고 합니다.
@@ -23,4 +25,20 @@
  */
 
 public class KnapsackA2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] dy = new int[m + 1];
+
+        for (int i = 0; i < n; i++) {
+            int ps = sc.nextInt();
+            int pt = sc.nextInt();
+
+            for (int j = m; j >= pt; j--) {
+                dy[j] = Math.max(dy[j], dy[j - pt] + ps);
+            }
+        }
+        System.out.println(dy[m]);
+    }
 }
