425378f7f85373d45a78616d913193666974c1e5
Lee JongSu <81428357+Lee-JongSoo@users.noreply.github.com>
2022-03-13 19:11:49 +0900

    Study: LIS Algorithm

--- DynamicProgramming/src/LIS.java
+++ DynamicProgramming/src/LIS.java
@@ -24,7 +24,17 @@ public class LIS {
 
     public int solution(int[] arr) {
         int answer  = 0;
-
+        dy = new int[arr.length];
+        dy[0] = 1;
+        answer = dy[0];
+        for (int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[j] < arr[i] && dy[j] > max) max = dy[j];
+            }
+            dy[i] = max + 1;
+            answer = Math.max(answer, dy[i]);
+        }
         return answer;
     }
 
